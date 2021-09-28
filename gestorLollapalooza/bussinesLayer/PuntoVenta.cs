6283e1278371f3ad1fa114a54b521d48a4431d79
Backhaus <tinchoignacio33@gmail.com>
2021-09-28 20:42:07 -0300

    abmc PuntoVenta PuntoVentaDao

---
+++ gestorLollapalooza/bussinesLayer/PuntoVenta.cs
@@ -0,0 +1,20 @@
+﻿using gestorLollapalooza.dataAccessLayer;
+
+namespace gestorLollapalooza.bussinesLayer
+{
+
+    public class PuntoVenta
+    {
+        public int IdPuntoVenta { get; set; }
+
+        public string NombrePuntoVenta { get; set; }
+
+        public int NumeroPuntoVenta { get; set; }
+        public string NombrePuntoVenta { get; set; }
+        public int idCentroVenta { get; set; }
+        public int BorradoLogico { get; set; }
+
+        public PuntoVenta() { }
+
+    }
+}
