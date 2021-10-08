2abd5a51dfcf3c7aa60a2be85677c4d03d43c7e1
Ignacio Perez Contreras <81949@sistemas.frc.utn.edu.ar>
2021-10-08 18:07:23 -0300

    hechas y completadas las clases de festival, dia festival, actuacion

---
+++ gestorLollapalooza/Service/FestivalService.cs
@@ -0,0 +1,46 @@
+﻿using gestorLollapalooza.bussinesLayer;
+using gestorLollapalooza.dataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestorLollapalooza.Service
+{
+    class FestivalService
+    {
+        private FestivalDao dao;
+        public IList<Festival> obtenerTodos()
+        {
+            return dao.obtenerTodos();
+        }
+
+        public bool borradoLogicamente(int idFestival)
+        {
+            return dao.borradoLogicamente(idFestival);
+        }
+
+        public bool modificarFestival(Festival festival)
+        {
+            return dao.modificarFestival(festival);
+        }
+
+        public bool persistirFestival(Festival festival)
+        {
+            return dao.persistirFestival(festival);
+        }
+
+        public IList<Festival> recuperarFiltrados(string filtros)
+        {
+            return dao.recuperarFiltrados(filtros);
+        }
+
+
+        public Festival MapearFestival(DataRow row)
+        {
+            return dao.MapearFestival(row);
+        }
+    }
+}
--- gestorLollapalooza/bussinesLayer/Festival.cs
+++ gestorLollapalooza/bussinesLayer/Festival.cs
@@ -8,5 +8,14 @@ namespace gestorLollapalooza.bussinesLayer
 {
     class Festival
     {
+        public int IdFestival { get; set; }
+        public int AnoEdicion { get; set; }
+        public int DescuentoVentaAnticipada { get; set; }
+        public int FechaInicio { get; set; }
+        public string Nombre { get; set; }
+        public int PorcentajeDevolucionPorAnulacion { get; set; }
+        public int Vigente { get; set; }
+        public IList<DiaFestival> Dias { get; set; }
+
     }
 }
