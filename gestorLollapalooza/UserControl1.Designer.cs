377174ad216f5d7bbbb1e0f28e081bdcc43adfb4
Backhaus <tinchoignacio33@gmail.com>
2021-10-09 12:24:25 -0300

    modelo de butacas

---
+++ gestorLollapalooza/UserControl1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace gestorLollapalooza
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonFacha1 = new gestorLollapalooza.BotonFacha();
+            this.botonFacha2 = new gestorLollapalooza.BotonFacha();
+            this.botonFacha3 = new gestorLollapalooza.BotonFacha();
+            this.botonFacha4 = new gestorLollapalooza.BotonFacha();
+            this.botonFacha5 = new gestorLollapalooza.BotonFacha();
+            this.SuspendLayout();
+            // 
+            // botonFacha1
+            // 
+            this.botonFacha1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.botonFacha1.BorderRadius = 20;
+            this.botonFacha1.BorderSize = 0;
+            this.botonFacha1.FlatAppearance.BorderSize = 0;
+            this.botonFacha1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonFacha1.ForeColor = System.Drawing.Color.White;
+            this.botonFacha1.Location = new System.Drawing.Point(77, 273);
+            this.botonFacha1.Name = "botonFacha1";
+            this.botonFacha1.Size = new System.Drawing.Size(150, 40);
+            this.botonFacha1.TabIndex = 0;
+            this.botonFacha1.Text = "Libre";
+            this.botonFacha1.TextColor = System.Drawing.Color.White;
+            this.botonFacha1.UseVisualStyleBackColor = false;
+            this.botonFacha1.Click += new System.EventHandler(this.botonFacha1_Click);
+            // 
+            // botonFacha2
+            // 
+            this.botonFacha2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha2.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.botonFacha2.BorderRadius = 16;
+            this.botonFacha2.BorderSize = 0;
+            this.botonFacha2.FlatAppearance.BorderSize = 0;
+            this.botonFacha2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonFacha2.ForeColor = System.Drawing.Color.White;
+            this.botonFacha2.Location = new System.Drawing.Point(86, 23);
+            this.botonFacha2.Name = "botonFacha2";
+            this.botonFacha2.Size = new System.Drawing.Size(8, 16);
+            this.botonFacha2.TabIndex = 1;
+            this.botonFacha2.Text = "botonFacha2";
+            this.botonFacha2.TextColor = System.Drawing.Color.White;
+            this.botonFacha2.UseVisualStyleBackColor = false;
+            // 
+            // botonFacha3
+            // 
+            this.botonFacha3.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha3.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha3.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.botonFacha3.BorderRadius = 20;
+            this.botonFacha3.BorderSize = 0;
+            this.botonFacha3.FlatAppearance.BorderSize = 0;
+            this.botonFacha3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonFacha3.ForeColor = System.Drawing.Color.White;
+            this.botonFacha3.Location = new System.Drawing.Point(282, 273);
+            this.botonFacha3.Name = "botonFacha3";
+            this.botonFacha3.Size = new System.Drawing.Size(150, 40);
+            this.botonFacha3.TabIndex = 2;
+            this.botonFacha3.Text = "Libre";
+            this.botonFacha3.TextColor = System.Drawing.Color.White;
+            this.botonFacha3.UseVisualStyleBackColor = false;
+            this.botonFacha3.Click += new System.EventHandler(this.botonFacha3_Click);
+            // 
+            // botonFacha4
+            // 
+            this.botonFacha4.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha4.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha4.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.botonFacha4.BorderRadius = 20;
+            this.botonFacha4.BorderSize = 0;
+            this.botonFacha4.FlatAppearance.BorderSize = 0;
+            this.botonFacha4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonFacha4.ForeColor = System.Drawing.Color.White;
+            this.botonFacha4.Location = new System.Drawing.Point(282, 340);
+            this.botonFacha4.Name = "botonFacha4";
+            this.botonFacha4.Size = new System.Drawing.Size(150, 40);
+            this.botonFacha4.TabIndex = 3;
+            this.botonFacha4.Text = "Libre";
+            this.botonFacha4.TextColor = System.Drawing.Color.White;
+            this.botonFacha4.UseVisualStyleBackColor = false;
+            this.botonFacha4.Click += new System.EventHandler(this.botonFacha4_Click);
+            // 
+            // botonFacha5
+            // 
+            this.botonFacha5.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha5.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.botonFacha5.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.botonFacha5.BorderRadius = 20;
+            this.botonFacha5.BorderSize = 0;
+            this.botonFacha5.FlatAppearance.BorderSize = 0;
+            this.botonFacha5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonFacha5.ForeColor = System.Drawing.Color.White;
+            this.botonFacha5.Location = new System.Drawing.Point(77, 340);
+            this.botonFacha5.Name = "botonFacha5";
+            this.botonFacha5.Size = new System.Drawing.Size(150, 40);
+            this.botonFacha5.TabIndex = 4;
+            this.botonFacha5.Text = "Libre";
+            this.botonFacha5.TextColor = System.Drawing.Color.White;
+            this.botonFacha5.UseVisualStyleBackColor = false;
+            this.botonFacha5.Click += new System.EventHandler(this.botonFacha5_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::gestorLollapalooza.Properties.Resources.Sala_de_cine;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.botonFacha5);
+            this.Controls.Add(this.botonFacha4);
+            this.Controls.Add(this.botonFacha3);
+            this.Controls.Add(this.botonFacha2);
+            this.Controls.Add(this.botonFacha1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(533, 404);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BotonFacha botonFacha1;
+        private BotonFacha botonFacha2;
+        private BotonFacha botonFacha3;
+        private BotonFacha botonFacha4;
+        private BotonFacha botonFacha5;
+    }
+}
---
+++ gestorLollapalooza/UserControl1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestorLollapalooza
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void botonFacha1_Click(object sender, EventArgs e)
+        {
+            botonFacha1.Text = "Ocùpado";
+            botonFacha1.BackColor = Color.Red;
+            botonFacha1.Enabled = false;
+               
+        }
+
+        private void botonFacha3_Click(object sender, EventArgs e)
+        {
+            botonFacha3.Text = "Ocùpado";
+            botonFacha3.BackColor = Color.Red;
+            botonFacha3.Enabled = false;
+        }
+
+        private void botonFacha5_Click(object sender, EventArgs e)
+        {
+            botonFacha5.Text = "Ocùpado";
+            botonFacha5.BackColor = Color.Red;
+            botonFacha5.Enabled = false;
+        }
+
+        private void botonFacha4_Click(object sender, EventArgs e)
+        {
+            botonFacha4.Text = "Ocùpado";
+            botonFacha4.BackColor = Color.Red;
+            botonFacha4.Enabled = false;
+        }
+    }
+}
